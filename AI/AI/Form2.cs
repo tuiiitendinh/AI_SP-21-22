7ed6430ff188ceeae65151930af3b19e254a0b8e
an5220001 <an5220001@gmail.com>
2021-12-06 22:41:52 +0700

    Vietnamese to English

--- AI/AI/Form2.cs
+++ AI/AI/Form2.cs
@@ -149,6 +149,120 @@ namespace AI
             }
         }
 
+        private void rjbutton1_Click(object sender, EventArgs e)
+        {
+            chisotmp = 0;
+            int x = 3, y = 3;
+            panel1.Controls.Clear();
+            Sodinh = trackBar1.Value;
+            for (int i = 0; i <= Sodinh; i++)
+            {
+                for (int j = 0; j <= Sodinh; j++)
+                {
+                    // tmp[i + j] = int.MinValue;
+                    mt[i, j] = int.MinValue;
+                    TextBox a = new TextBox();
+                    a.Multiline = true;
+                    a.Width = 50;
+                    a.Height = h;
+                    if (i == 0 && j == 0)
+                    {
+                        a.ReadOnly = true;
+                        a.Text = $"*";
+                    }
+                    else
+                    {
+                        if (i == 0)
+                        {
+                            a.ReadOnly = true;
+                            a.Text = $"N{j - 1}";
+                        }
+                        if (j == 0)
+                        {
+                            a.ReadOnly = true;
+                            a.Text = $"N{i - 1}";
+                        }
+                        if (i == j)
+                        {
+                            a.ReadOnly = true;
+                            a.Text = $"∞";
+                        }
+                        if (i > j && j != 0)
+                        {
+                            a.ReadOnly = true;
+                            a.Text = "???";
+                        }
+                    }
+                    a.Location = new Point(x, y);
+                    panel1.Controls.Add(a);
+                    x += m + 50;
+                }
+                x = 3;
+                y += m + h;
+            }
+        }
+
+        private void rjbutton2_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox textBox in panel1.Controls)
+            {
+                if (!textBox.ReadOnly)
+                {
+                    int trongso = rnd.Next(-700, 1001);
+                    if (trongso < 0) textBox.Text = "∞";
+                    else textBox.Text = trongso.ToString();
+
+                }
+            }
+        }
+
+        private void rjbutton3_Click(object sender, EventArgs e)
+        {
+            kt = true;
+            foreach (TextBox textBox in panel1.Controls)
+            {
+                int trongso;
+                if (!textBox.ReadOnly)
+                {
+                    try
+                    {
+
+                        if (textBox.Text == "∞" || int.Parse(textBox.Text) < 0) trongso = int.MinValue;
+                        else trongso = int.Parse(textBox.Text);
+                        tmp[chisotmp++] = trongso;
+                    }
+                    catch
+                    {
+                        kt = false;
+                        MessageBox.Show("Invalid Matrix", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+            }
+            if (kt)
+            {
+                chisotmp = 0;
+                for (int i = 0; i < Sodinh - 1; i++)
+                {
+                    for (int j = i + 1; j < Sodinh; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (tmp[chisotmp] == int.MinValue) mt[i, j] = mt[j, i] = int.MinValue;
+                            else
+                            {
+
+                                mt[i, j] = mt[j, i] = tmp[chisotmp];
+                            }
+                            chisotmp++;
+                        }
+                    }
+                }
+                chisotmp = 0;
+                this.Close();
+            }
+        }
+
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             so_dinh.Text = trackBar1.Value.ToString();
